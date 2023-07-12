c86673bbfe691cf5247a1bf10764e03a72a90cd0
sylvie <1553902582@qq.com>
2023-07-12 11:10:10 +0800

    项目初始化

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import electron from 'vite-plugin-electron'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import electron from "vite-plugin-electron";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     electron({
-      entry: 'electron/main.ts'
-    })
+      entry: "electron/main.ts",
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
